4784a08ce082284219953e75a38dffbb48076fca
WWWorm <thewwworm@gmail.com>
2025-08-14 12:48:47 +0300

    initial MVP

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Folder with your videos
+const VIDEO_DIR = path.join(__dirname, "videos");
+
+// Serve static files (videos & client page)
+app.use("/videos", express.static(VIDEO_DIR));
+
+// Endpoint to get video list
+app.get("/list", (req, res) => {
+  fs.readdir(VIDEO_DIR, (err, files) => {
+    if (err) return res.status(500).json({ error: err.message });
+
+    const videos = files.filter(f =>
+      [".mkv"].includes(path.extname(f).toLowerCase())
+    );
+
+    res.json(videos);
+  });
+});
+
+// Serve the HTML player
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
